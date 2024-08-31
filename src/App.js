fde8fd2b92f6438b46d744dc29cf7d500b925881
sumita <sumitabalouch@gmail.com>
2024-08-31 18:30:39 +0430

    added new functionality for the audio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,116 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { setTimer } from './redux/actions';
-import Timer from './components/Timer';
-import Controls from './components/Controls';
-import Buttons from './components/Buttons';
+import { setTimer, toggleTimer, setBreakLength, setSessionLength, toggleSession, resetTimer } from './redux/actions';
 
 function App() {
-  // ... rest of your App component code
+  const dispatch = useDispatch();
+  const { timer, isRunning, isSession, breakLength, sessionLength } = useSelector(state => state);
+  const [timerLabel, setTimerLabel] = useState('Session');
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    let interval = null;
+    if (isRunning && timer > 0) {
+      interval = setInterval(() => {
+        dispatch(setTimer(timer - 1));
+      }, 1000);
+    } else if (timer === 0) {
+      if (audioRef.current) {
+        audioRef.current.play();
+      }
+      if (isSession) {
+        dispatch(toggleSession());
+        dispatch(setTimer(breakLength * 60));
+        setTimerLabel('Break');
+      } else {
+        dispatch(toggleSession());
+        dispatch(setTimer(sessionLength * 60));
+        setTimerLabel('Session');
+      }
+    }
+    return () => clearInterval(interval);
+  }, [isRunning, timer, isSession, breakLength, sessionLength, dispatch]);
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+  };
+
+  const handleBreakDecrement = () => {
+    if (breakLength > 1 && !isRunning) {
+      dispatch(setBreakLength(breakLength - 1));
+    }
+  };
+
+  const handleBreakIncrement = () => {
+    if (breakLength < 60 && !isRunning) {
+      dispatch(setBreakLength(breakLength + 1));
+    }
+  };
+
+  const handleSessionDecrement = () => {
+    if (sessionLength > 1 && !isRunning) {
+      dispatch(setSessionLength(sessionLength - 1));
+      if (isSession) {
+        dispatch(setTimer((sessionLength - 1) * 60));
+      }
+    }
+  };
+
+  const handleSessionIncrement = () => {
+    if (sessionLength < 60 && !isRunning) {
+      dispatch(setSessionLength(sessionLength + 1));
+      if (isSession) {
+        dispatch(setTimer((sessionLength + 1) * 60));
+      }
+    }
+  };
+
+  const handleStartStop = () => {
+    dispatch(toggleTimer());
+  };
+
+  const handleReset = () => {
+    dispatch(resetTimer());
+    setTimerLabel('Session');
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
+    if (isRunning) {
+      dispatch(toggleTimer());
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1>25 + 5 Clock</h1>
+      <div className="length-controls">
+        <div>
+          <div id="break-label">Break Length</div>
+          <button id="break-decrement" onClick={handleBreakDecrement}>-</button>
+          <span id="break-length">{breakLength}</span>
+          <button id="break-increment" onClick={handleBreakIncrement}>+</button>
+        </div>
+        <div>
+          <div id="session-label">Session Length</div>
+          <button id="session-decrement" onClick={handleSessionDecrement}>-</button>
+          <span id="session-length">{sessionLength}</span>
+          <button id="session-increment" onClick={handleSessionIncrement}>+</button>
+        </div>
+      </div>
+      <div className="timer-display">
+        <div id="timer-label">{timerLabel}</div>
+        <div id="time-left">{formatTime(timer)}</div>
+      </div>
+      <button id="start_stop" onClick={handleStartStop}>
+        {isRunning ? 'Stop' : 'Start'}
+      </button>
+      <button id="reset" onClick={handleReset}>Reset</button>
+      <audio id="beep" ref={audioRef} src="path_to_your_audio_file.mp3" />
+    </div>
+  );
 }
 
 export default App;
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,9 @@
-import { SET_BREAK_LENGTH, SET_SESSION_LENGTH, SET_TIMER, RESET_TIMER, TOGGLE_TIMER } from './actions';
+import { SET_BREAK_LENGTH, SET_SESSION_LENGTH, SET_TIMER, RESET_TIMER, TOGGLE_TIMER, TOGGLE_SESSION } from './actions';
 
 const initialState = {
   breakLength: 5,
   sessionLength: 25,
-  timer: 25 * 60,
+  timer: 25 * 60, // Timer starts with the session length
   isSession: true,
   isRunning: false,
 };
@@ -13,13 +13,27 @@ const reducer = (state = initialState, action) => {
     case SET_BREAK_LENGTH:
       return { ...state, breakLength: action.payload };
     case SET_SESSION_LENGTH:
-      return { ...state, sessionLength: action.payload, timer: action.payload * 60 };
+      // When session length is updated, reset the timer if currently in session
+      return {
+        ...state,
+        sessionLength: action.payload,
+        timer: state.isSession ? action.payload * 60 : state.timer
+      };
     case SET_TIMER:
       return { ...state, timer: action.payload };
     case RESET_TIMER:
-      return { ...initialState };
+      return {
+        ...initialState,
+        timer: initialState.sessionLength * 60, // Reset timer to initial session length
+      };
     case TOGGLE_TIMER:
       return { ...state, isRunning: !state.isRunning };
+    case TOGGLE_SESSION:
+      return {
+        ...state,
+        isSession: !state.isSession,
+        timer: state.isSession ? state.breakLength * 60 : state.sessionLength * 60
+      };
     default:
       return state;
   }
