3036db67e64fa19801d983e68f6a2bb279d19388
sumita <sumitabalouch@gmail.com>
2024-09-01 18:00:42 +0430

    solving the linters errors in buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -23,8 +23,9 @@ const Component3 = () => {
       <button id="start_stop" type="button" onClick={handleStartStop}>Start/Stop</button>
       <button id="reset" type="button" onClick={handleReset}>Reset</button>
       <audio id="beep" ref={audioRef} src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav">
+       <track kind="none" />
+         </audio>
         <track kind="metadata" />
-      </audio>
     </div>
   );
 };
